2a81f511ae0c0c80bd7a2d7f5cdc932967b91c7e
febonatto <febonatto_1999@hotmail.com>
2021-05-20 18:40:45 -0300

    Adicionado back_end

---
+++ spring-app/src/test/java/br/com/usjt/collegeproject/TelemedicinaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.usjt.collegeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedicinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

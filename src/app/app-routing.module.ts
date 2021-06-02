64e4bebf9d9c5a28abfde0e703002c6e322fd82e
Pedro Rocha <pedrohrrf17@gmail.com>
2021-06-02 18:04:56 -0300

    Tela cadastro NavBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { RegisterComponent } from './template/cadastro/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,25 @@ import { AppComponent } from './app.component';
 
 import { RegisterComponent } from './template/cadastro/register/register.component';
 import { MedicoComponent } from './template/cadastro/medico/medico.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
-  declarations: [AppComponent, RegisterComponent, MedicoComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    MedicoComponent,
+    CabecalhoComponent,
+    IndexComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
